f01118e09ed090bfa854c640b98a9756dacbc2d8
Sherman5A <83960518+Sherman5A@users.noreply.github.com>
2021-09-09 09:16:13 +0100

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,6 @@
+import sql
+
+def password_check(input_username, input_password):
+  password = sql.get(password)
+  username = sql.get(username)
+  return password == input_passsword and username = input_username 
