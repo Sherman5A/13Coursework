f1ebffb6f8f6850f20b4402dac72ee0b43742ed5
Jake <jakereal2004@gmail.com>
2021-09-14 12:29:49 +0100

    add: validation, sign up

--- logic.py
+++ logic.py
@@ -1,4 +1,36 @@
 import SQL_interface_2
+import re
+
 
 def check_login_creds(input_username, input_password):
     pass
+
+
+def validate_alpha(input):
+    
+    regex_arguement = re.compile('\d+|[@;:()]')
+    try:
+        regex_return = re.search(regex_arguement, input)    
+        if regex_return:
+            return False
+        return True
+    except TypeError:
+        print('Variable is not string')
+    
+
+def validate_num(input, min_num=None):
+    try:
+        converted_input = int(input)
+        if min_num == None:
+            return True
+        return converted_input > min_num
+    except ValueError:
+        return False
+
+def validate_date(input):
+    pass
+
+
+
+print(validate_alpha(25))
+print(validate_num(-5,0))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import logic
 import tkinter as tk
-from tkinter.constants import LEFT
+from tkinter.constants import COMMAND, LEFT
 
 
 class Gui(tk.Tk):
@@ -18,7 +18,7 @@ class Gui(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, TextLogin):
+        for F in (StartPage, TextLogin, SignUp):
             frame = F(parent=container, controller=self)
             self.frames[F.__name__] = frame
             frame.grid(row=0, column=0, sticky='nsew')
@@ -43,14 +43,20 @@ class StartPage(tk.Frame):
         lbl_title.pack()
         self.btn_start_login = tk.Button(self, text='Login Page', command=lambda: self.controller.show_frame('TextLogin'))
         self.btn_start_login.pack()
+
+        self.btn_signup = tk.Button(self, text='Sign up', command=lambda: self.controller.show_frame('SignUp'))
+        self.btn_signup.pack()
         
 
 
 class TextLogin(tk.Frame):
 
     def __init__(self, parent, controller):
+        # initialise frame
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+        # Create GUI elements
         lbl_page_title = tk.Label(self, text='Login')
         lbl_page_title.pack()
         self.input_frame = tk.Frame(self)
@@ -80,7 +86,68 @@ class TextLogin(tk.Frame):
         else:
             pass
 
+class SignUp(tk.Frame):
 
+    def __init__(self, parent, controller):
+        '''Initialise class values and create GUI elements'''
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        
+        # gui creation
+        lbl_signup_title = tk.Label(self, text='Sign up')
+        lbl_signup_title.pack(pady=10)
+
+        user_input_frame = tk.Frame(self)
+        user_input_frame.pack()
+
+
+        lbl_first_name = tk.Label(user_input_frame, text='First Name:')
+        lbl_first_name.grid(row=0, column=0, pady=4)
+
+        ent_first_name = tk.Entry(user_input_frame)
+        ent_first_name.grid(row=0, column=1, pady=4)
+
+        lbl_second_name = tk.Label(user_input_frame, text='Last name:')
+        lbl_second_name.grid(row=1, column=0, pady=4)
+
+        lbl_second_name = tk.Entry(user_input_frame)
+        lbl_second_name.grid(row=1, column=1, pady=4)
+
+        year_groups = ['12', '13']
+        year_value = tk.StringVar(user_input_frame, value='Select a year group')
+
+        menu_year_group = tk.OptionMenu(user_input_frame, year_value, *year_groups)
+        menu_year_group.grid(row=2, column=0, sticky='ew', pady=3, columnspan=2)
+
+        
+        form_value = tk.StringVar(user_input_frame, value='Select a form group')
+        form_list = ['A', 'B', 'C', 'D', 'E', 'D', 'F']
+        
+        menu_form_group = tk.OptionMenu(user_input_frame, form_value, *form_list)
+        menu_form_group.grid(row=3, column=0, sticky='ew', pady=3, columnspan=2)
+
+        lbl_username = tk.Label(user_input_frame, text='Username:')
+        lbl_username.grid(row=4, column=0, pady=3)
+
+        ent_username = tk.Entry(user_input_frame)
+        ent_username.grid(row=4, column=1, pady=3)
+
+        #<3 <3
+
+        lbl_password = tk.Label(user_input_frame, text='Password')
+        lbl_password.grid(row=5, column=0, pady=3)
+
+        ent_password = tk.Entry(user_input_frame, show='*')
+        ent_password.grid(row=5, column=1, pady=3)
+
+        lbl_password_repeat = tk.Label(user_input_frame, text='Repeat Password')
+        lbl_password_repeat.grid(row=6, column=0, pady=3)
+
+        ent_password_repeat = tk.Entry(user_input_frame, show='*')
+        ent_password_repeat.grid(row=6, column=1, pady=3)
+
+
+        
 
 if __name__ == '__main__':
     
